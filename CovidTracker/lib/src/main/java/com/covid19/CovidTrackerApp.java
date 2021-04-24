1b0146e8a4e5498b8d3bb75097565b67f23bf2e0
surraut <surraut@LIN25003946.corp.capgemini.com>
2021-04-24 17:08:41 +0530

    This is the first Commit

---
+++ CovidTracker/lib/src/main/java/com/covid19/CovidTrackerApp.java
@@ -0,0 +1,16 @@
+package com.covid19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.sun.tools.javac.Main;
+
+@SpringBootApplication
+public class CovidTrackerApp {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(Main.class, args);
+	}
+
+}
